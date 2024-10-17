8d5e0488c3e4eb792c2c8c69f7c82a68178ad452
nelsongei <psammy.ngei@gmail.com>
2024-10-17 09:03:16 +0300

    updates on data

--- app/Http/Controllers/Api/JobsApiController.php
+++ app/Http/Controllers/Api/JobsApiController.php
@@ -27,8 +27,78 @@ class JobsApiController extends Controller
         $ids = $hash->decode($id)[0];
         return Job::findOrFail($ids);
     }
-
     public function store(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            // Create and save Application
+            $application = new Application();
+            $application->job_id = $request->id;
+            $application->status_id = 1;
+            $application->full_name = $request->fullname;
+            $application->id_no = $request->id_number;
+            $application->email = $request->email;
+            $application->phone_number = $request->mobile_no;
+            $application->gender = $request->gender;
+            $application->language = $request->language;
+            $application->years_of_experience = $request->experience_years;
+            $application->work_experience = $request->experience_details;
+            $application->notify_hr = true;
+            $application->notify_applicant = true;
+            $application->save();
+
+            // Bulk insert referees
+            $refereesData = [];
+            foreach ($request->referee_name as $index => $full_name) {
+                $refereesData[] = [
+                    'application_id' => $application->id,
+                    'full_name' => $full_name,
+                    'contact' => $request->referee_contact[$index],
+                    'consent' => $request->referee_consent[$index]
+                ];
+            }
+            Referee::insert($refereesData);
+
+            // Check and store academic files
+            if ($request->hasFile('academic_file')) {
+                $attachmentsData = [];
+                foreach ($request->academic_level as $index => $level) {
+                    if (isset($request->file('academic_file')[$index])) {
+                        $filePath = $request->file('academic_file')[$index]->store('application_attachment', 'public');
+                        $attachmentsData[] = [
+                            'application_id' => $application->id,
+                            'document' => $filePath,
+                            'level' => $level,
+                            'grade' => $request->academic_grade[$index],
+                            'duration' => $request->academic_duration[$index]
+                        ];
+                    }
+                }
+                ApplicationAttachment::insert($attachmentsData);
+            }
+
+            // Bulk insert professional qualifications
+            $qualificationsData = [];
+            foreach ($request->qualification as $index => $qualification) {
+                $qualificationsData[] = [
+                    'application_id' => $application->id,
+                    'name' => $qualification,
+                    'grade' => $request->qualification_grade[$index],
+                    'duration' => $request->qualification_duration[$index]
+                ];
+            }
+            ProfessionQualification::insert($qualificationsData);
+
+            DB::commit();
+            return response()->json(['message' => 'Successfully created application'], 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function store1(Request $request)
     {
         try {
             DB::beginTransaction();
@@ -77,9 +147,6 @@ class JobsApiController extends Controller
                     }
                 }
             }
-
-
-//        $key=>$value
             foreach ($request->qualification as $index => $qualification) {
                 // Store ProfessionQualification
                 $professionQualification = new ProfessionQualification();
@@ -90,12 +157,10 @@ class JobsApiController extends Controller
                 $professionQualification->save();
             }
             DB::commit();
-            return response()->json(['messege' => 'Success']);
+            return response()->json(['message' => 'Successfully ']);
         } catch (\Exception $e) {
             DB::rollBack();
-            return response()->json($e->getMessage());
+            return response()->json($e->getMessage(),500);
         }
     }
-
-
 }
