5843337fde61e095ce98f0889017b3cda81f8290
nelsongei <psammy.ngei@gmail.com>
2024-10-18 12:51:33 +0300

    updates

--- resources/js/Pages/Admin/Index.vue
+++ resources/js/Pages/Admin/Index.vue
@@ -5,39 +5,41 @@
         </AuthenticatedLayout>
         <!-- Main Content -->
         <main>
-            <Header/>
-            <section class="dashboard">
-                <h1>Administration</h1>
-                <div class="card">
-                    <div class="card-body">
-                        <button class="btn">Add Admin</button>
-                        <section class="table-section">
-                            <table>
-                                <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Role</th>
-                                    <th>Action</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <template v-for="(user,index) in users" :key="user.id">
+            <div>
+                <Header/>
+                <section class="dashboard">
+                    <h1>Administration</h1>
+                    <div class="card">
+                        <div class="card-body">
+                            <button class="btn">Add Admin</button>
+                            <section class="table-section">
+                                <table>
+                                    <thead>
                                     <tr>
-                                        <td>{{index+1}}</td>
-                                        <td>{{user.firstname+' '+user.lastname}}</td>
-                                        <td>{{user.email}}</td>
-                                        <td>Admin</td>
-                                        <td>Action</td>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Role</th>
+                                        <th>Action</th>
                                     </tr>
-                                </template>
-                                </tbody>
-                            </table>
-                        </section>
+                                    </thead>
+                                    <tbody>
+                                    <template v-for="(user,index) in users" :key="user.id">
+                                        <tr>
+                                            <td>{{index+1}}</td>
+                                            <td>{{user.firstname+' '+user.lastname}}</td>
+                                            <td>{{user.email}}</td>
+                                            <td>Admin</td>
+                                            <td>Action</td>
+                                        </tr>
+                                    </template>
+                                    </tbody>
+                                </table>
+                            </section>
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
             <Footer/>
         </main>
     </div>
