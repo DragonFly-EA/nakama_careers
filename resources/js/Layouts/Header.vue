7f7f4f9c32d1826e5759eeae2019a4257d547dc1
nelsongei <psammy.ngei@gmail.com>
2024-10-18 15:26:02 +0300

    updates on data

--- resources/js/Layouts/Header.vue
+++ resources/js/Layouts/Header.vue
@@ -1,8 +1,23 @@
 <script>
 import { Head } from '@inertiajs/vue3';
+export default {
+    data(){
+        return{
+            showSidebar: false,
+        }
+    },
+    methods: {
+        showSidebarMethod(){
+            $(".sidebar").show();
+        }
+    }
+}
 </script>
 <template>
     <header class="main-header">
+        <button class="menu-bars" @click="showSidebarMethod">
+            <i class="fa fa-bars"></i>
+        </button>
         <div class="search-bar">
             <input type="text" placeholder="Search here...">
             <i class="fas fa-search search-icon"></i>
@@ -25,3 +40,4 @@ import { Head } from '@inertiajs/vue3';
 <!--        <button class="post-job-btn"><i class="fas fa-plus-circle"></i> Post a Job</button>-->
     </header>
 </template>
+
