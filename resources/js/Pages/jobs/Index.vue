cf571d0f5ff79531caf6782a9b85d2d44cff2656
nelsongei <psammy.ngei@gmail.com>
2024-10-18 13:05:25 +0300

    jobs mobile

--- resources/js/Pages/jobs/Index.vue
+++ resources/js/Pages/jobs/Index.vue
@@ -9,68 +9,103 @@
                 <Header/>
                 <section class="dashboard">
                     <h1>Jobs</h1>
-                    <div class="card">
-                        <div class="card-body">
-                            <Link :href="route('jobs.create')" class="btn">Post a Job</Link>
-                            <section class="table-section">
-                                <table>
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Title</th>
-                                        <th>Location</th>
-                                        <th>Description</th>
-                                        <th>Requirements</th>
-                                        <th>Expires on</th>
-                                        <th>Status</th>
-                                        <th>Action</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <template v-if="jobs.length>0">
-                                        <tr v-for="(job,index) in jobs" :key="job.id">
-                                            <td>{{index+1}}</td>
-                                            <td>
-                                                <Link :href="route('jobs.view',job.id)">
-                                                    {{job.title}}
-                                                </Link>
-                                            </td>
-                                            <td>{{job.location}}</td>
-                                            <td v-html="truncatedData(job.description)"></td>
-                                            <td v-html="truncatedData(job.requirements)"></td>
-                                            <td>{{job.expires_on}}</td>
-                                            <td>
-                                                <template v-if="job.status===1">
-                                                    Active
-                                                </template>
-                                                <template v-else>
-                                                    Inactive
-                                                </template>
-                                            </td>
-                                            <td>
-                                                <div class="dropdown">
-                                                    <button class="dropbtn">Action<i class="fa fa-caret-down"></i></button>
-                                                    <div class="dropdown-content">
-                                                        <Link :href="route('jobs.view',job.id)">View</Link>
-                                                        <Link :href="route('jobs.edit',job.id)">Edit</Link>
-                                                        <a href="#">Archive</a>
-                                                        <a href="#">Delete</a>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </template>
-                                    <template v-else>
+                    <div class="desktop-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <Link :href="route('jobs.create')" class="btn">Post a Job</Link>
+                                <section class="table-section">
+                                    <table>
+                                        <thead>
                                         <tr>
-                                            <td colspan="8" style="text-align: center;color: var(--color1)">
-                                                <i class="fa fa-copy fa-5x"></i>
-                                            </td>
+                                            <th>#</th>
+                                            <th>Title</th>
+                                            <th>Location</th>
+                                            <th>Description</th>
+                                            <th>Requirements</th>
+                                            <th>Expires on</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
                                         </tr>
-                                    </template>
-                                    </tbody>
-                                </table>
-                            </section>
+                                        </thead>
+                                        <tbody>
+                                        <template v-if="jobs.length>0">
+                                            <tr v-for="(job,index) in jobs" :key="job.id">
+                                                <td>{{ index + 1 }}</td>
+                                                <td>
+                                                    <Link :href="route('jobs.view',job.id)">
+                                                        {{ job.title }}
+                                                    </Link>
+                                                </td>
+                                                <td>{{ job.location }}</td>
+                                                <td v-html="truncatedData(job.description)"></td>
+                                                <td v-html="truncatedData(job.requirements)"></td>
+                                                <td>{{ job.expires_on }}</td>
+                                                <td>
+                                                    <template v-if="job.status===1">
+                                                        Active
+                                                    </template>
+                                                    <template v-else>
+                                                        Inactive
+                                                    </template>
+                                                </td>
+                                                <td>
+                                                    <div class="dropdown">
+                                                        <button class="dropbtn">Action<i class="fa fa-caret-down"></i>
+                                                        </button>
+                                                        <div class="dropdown-content">
+                                                            <Link :href="route('jobs.view',job.id)">View</Link>
+                                                            <Link :href="route('jobs.edit',job.id)">Edit</Link>
+                                                            <a href="#">Archive</a>
+                                                            <a href="#">Delete</a>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </template>
+                                        <template v-else>
+                                            <tr>
+                                                <td colspan="8" style="text-align: center;color: var(--color1)">
+                                                    <i class="fa fa-copy fa-5x"></i>
+                                                </td>
+                                            </tr>
+                                        </template>
+                                        </tbody>
+                                    </table>
+                                </section>
+                            </div>
                         </div>
+                    </div>
+                    <div class="mobile-card">
+                        <ul>
+                            <template v-if="jobs.length>0">
+                                <li v-for="(job,index) in jobs" :key="job.id">
+                                    <h1>{{ job.title }}</h1>
+                                    <h1>{{ job.location }}</h1>
+                                    <h1>{{ job.expires_on }}</h1>
+                                    <h1>
+                                        <template v-if="job.status===1">
+                                            Active
+                                        </template>
+                                        <template v-else>
+                                            Inactive
+                                        </template>
+                                    </h1>
+                                    <div class="dropdown">
+                                        <button class="dropbtn">Action<i class="fa fa-caret-down"></i>
+                                        </button>
+                                        <div class="dropdown-content">
+                                            <Link :href="route('jobs.view',job.id)">View</Link>
+                                            <Link :href="route('jobs.edit',job.id)">Edit</Link>
+                                            <a href="#">Archive</a>
+                                            <a href="#">Delete</a>
+                                        </div>
+                                    </div>
+                                </li>
+                            </template>
+                            <template v-else>
+                                <li>Failed</li>
+                            </template>
+                        </ul>
                     </div>
                 </section>
             </div>
@@ -87,12 +122,12 @@ import Footer from "@/Layouts/Footer.vue";
 </script>
 <script>
 export default {
-    props:{
-        jobs:[],
+    props: {
+        jobs: [],
     },
-    methods:{
-        truncatedData(description){
-            return description.substring(0,150)+'...';
+    methods: {
+        truncatedData(description) {
+            return description.substring(0, 150) + '...';
         }
     },
 }
