6da84570e07e8589a62ce79eb7919adf24f60790
nelsongei <psammy.ngei@gmail.com>
2024-10-22 13:43:59 +0300

    api update

--- app/Http/Controllers/Api/JobsApiController.php
+++ app/Http/Controllers/Api/JobsApiController.php
@@ -92,7 +92,7 @@ class JobsApiController extends Controller
             ProfessionQualification::insert($qualificationsData);
 
             DB::commit();
-            return response()->json(['message' => 'Successfully created application'], 200);
+            return response()->json(['message' => 'Successfully created application','status'=>200], 200);
         } catch (\Exception $e) {
             DB::rollBack();
             return response()->json(['error' => $e->getMessage()], 500);
