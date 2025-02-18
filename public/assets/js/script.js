bf3f0e1989e2e13bfa8baa53d81728650ff6b50e
nelsongei <psammy.ngei@gmail.com>
2025-02-18 15:57:53 +0300

    updates

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,39 +1,39 @@
-const ctx = document.getElementById('jobViewsChart').getContext('2d');
-const jobViewsChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        datasets: [{
-            label: 'Job Views',
-            data: [50, 100, 200, 150, 250, 150, 100],
-            fill: true,
-            backgroundColor: 'rgba(85, 230, 115, 0.2)',
-            borderColor: '#55E673',
-            borderWidth: 2,
-            pointBackgroundColor: '#55E673',
-            pointBorderColor: '#fff',
-            pointRadius: 5,
-            tension: 0.3
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-                grid: {
-                    color: '#ddd'
-                }
-            },
-            x: {
-                grid: {
-                    display: false
-                }
-            }
-        },
-        plugins: {
-            legend: {
-                display: false
-            }
-        }
-    }
-});
+// const ctx = document.getElementById('jobViewsChart').getContext('2d');
+// const jobViewsChart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+//         datasets: [{
+//             label: 'Job Views',
+//             data: [50, 100, 200, 150, 250, 150, 100],
+//             fill: true,
+//             backgroundColor: 'rgba(85, 230, 115, 0.2)',
+//             borderColor: '#55E673',
+//             borderWidth: 2,
+//             pointBackgroundColor: '#55E673',
+//             pointBorderColor: '#fff',
+//             pointRadius: 5,
+//             tension: 0.3
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true,
+//                 grid: {
+//                     color: '#ddd'
+//                 }
+//             },
+//             x: {
+//                 grid: {
+//                     display: false
+//                 }
+//             }
+//         },
+//         plugins: {
+//             legend: {
+//                 display: false
+//             }
+//         }
+//     }
+// });
